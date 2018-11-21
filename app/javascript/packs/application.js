2668ebd53ba726a6137c41d0014a8d15102e3a24
Louis Theroux <louistheroux@live.com>
2018-11-21 13:59:29 +0100

    Add fullcalendar to project with some config

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,18 @@
 import "bootstrap";
+import { Calendar } from 'fullcalendar';
+import { foo } from '../components/foo'
+import 'fullcalendar/dist/fullcalendar.min.css';
+// import $ from 'jquery'
+foo()
+document.addEventListener('DOMContentLoaded', function() { // page is now ready...
+  var calendarEl = document.getElementById('calendar'); // grab element reference
+
+  var calendar = new Calendar(calendarEl, {
+    locale: "en-gb"
+    // weekends: false
+  });
+
+  calendar.render();
+});
+
+// window.$ = $
